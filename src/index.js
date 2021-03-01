43815f8827762fc3a5647bc9c6f772cfa395f328
Victor Yurchenko <vic.yurchenko@yandex.ru>
2021-03-01 21:34:14 +0700

    React Components

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const TodoList = () => {
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  const items = ['Buy Azelit', 'Buy sunflowers oil']
+
+  return (
+    <ul>
+      <li>{ items[0] }</li>
+      <li>{ items[1] }</li>
+    </ul>
+  );
+};
+
+const AppHeader = () => {
+  return <h1>My Todo List</h1>;
+};
+
+const SearchPanel = () => {
+  return <input placeholder="search"/>;
+}
+
+const App = () => {
+  return (
+    <div>
+      <AppHeader/>
+      <SearchPanel/>
+      <TodoList/>
+    </div>
+  );
+}
+
+ReactDOM.render(<App/>, document.getElementById("root"));
