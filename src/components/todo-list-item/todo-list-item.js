469ddc9758dbc89bbbd5e075f7e92a1fd8eae1b3
Victor Yurchenko <vik@4vrs.com>
2021-03-04 14:08:02 +0700

    components as classes

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -1,32 +1,36 @@
-import React from "react";
+import React, {Component} from "react";
 import "./todo-list-item.css"
 
-const TodoListItem = ( {label, important = false} ) => {
+export default class TodoListItem extends Component {
 
-  const style = {
-    color: important ? 'steelblue' : 'black',
-    fontWeight: important ? 'bold' : 'normal',
-  };
+    onLabelClick = () => {
+        console.log(`Done: ${this.props.label}`);
+    };
 
-  return (
-    <span className="todo-list-item">
+    render(){
+        let {label, important = false} = this.props
+        const style = {
+            color: important ? 'steelblue' : 'black',
+            fontWeight: important ? 'bold' : 'normal',
+        };
+        return (
+            <span className="todo-list-item">
+              <span
+                  className="todo-list-item-label"
+                  style={style}
+                  onClick={this.onLabelClick}
+              >
+                {label}
+              </span>
+              <button type="button"
+                      className="btn btn-outline-success btn-sm float-right">
+                <i className="fa fa-exclamation"/>
+              </button>
+               <button type="button"
+                       className="btn btn-outline-danger btn-sm float-right">
+                <i className="fa fa-trash-o" />
+              </button>
+            </span>);
+    }
+}
 
-      <span
-        className="todo-list-item-label"
-        style={style}>
-        {label}
-      </span>
-
-      <button type="button"
-              className="btn btn-outline-success btn-sm float-right">
-        <i className="fa fa-exclamation"/>
-      </button>
-
-       <button type="button"
-               className="btn btn-outline-danger btn-sm float-right">
-        <i className="fa fa-trash-o" />
-      </button>
-    </span>);
-};
-
-export default TodoListItem;
