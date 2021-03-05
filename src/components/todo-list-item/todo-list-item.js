4f4c00600949f603770c096b125f7fe2d03963d2
Victor Yurchenko <vik@4vrs.com>
2021-03-05 14:04:06 +0700

    logic to set state

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -3,27 +3,42 @@ import "./todo-list-item.css"
 
 export default class TodoListItem extends Component {
 
+
+    state = {
+        done: false,
+        important :false,
+    };
+
     onLabelClick = () => {
-        console.log(`Done: ${this.props.label}`);
+        this.setState(({done}) => {
+            return { done: !done }
+        });
+    };
+
+    omMarkImportant = () => {
+        this.setState(({important}) => {
+           return  { important: !important }
+        });
     };
 
     render(){
-        let {label, important = false} = this.props
-        const style = {
-            color: important ? 'steelblue' : 'black',
-            fontWeight: important ? 'bold' : 'normal',
-        };
+        const {label} = this.props;
+        const {done, important} = this.state;
+        let classNames = 'todo-list-item'
+        classNames += done ? ' done' : '';
+        classNames += important ? ' important' : '';
         return (
-            <span className="todo-list-item">
+            <span className={classNames}>
               <span
                   className="todo-list-item-label"
-                  style={style}
                   onClick={this.onLabelClick}
               >
                 {label}
               </span>
               <button type="button"
-                      className="btn btn-outline-success btn-sm float-right">
+                      className="btn btn-outline-success btn-sm float-right"
+                      onClick={this.omMarkImportant}
+              >
                 <i className="fa fa-exclamation"/>
               </button>
                <button type="button"
